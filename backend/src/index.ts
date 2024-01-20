fb1c7d21603e260e9f957394d64e525dc552b85a
rikcav <henriquearau000@gmail.com>
2024-01-20 13:38:25 -0300

    chore: adaptando o backend para typescript

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello World!');
 });
 

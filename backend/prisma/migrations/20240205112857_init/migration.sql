a15ff27e4e1bf68904a42920379fe818a25cb0ea
rikcav <henriquearau000@gmail.com>
2024-02-05 08:29:30 -0300

    feat: migration inicial

---
+++ backend/prisma/migrations/20240205112857_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");

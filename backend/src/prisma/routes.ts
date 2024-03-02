8eb532f6f68c090bb89efc9c1f9da5d3c499c44b
rikcav <henriquearau000@gmail.com>
2024-03-02 15:21:30 -0300

    feat: cadastro das rotas de item no app

--- backend/src/prisma/routes.ts
+++ backend/src/prisma/routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { userRoutes } from "../user/routes";
+import { itemRoutes } from "../item/routes";
 
 module.exports = (app: express.Application) => {
   userRoutes(app);
+  itemRoutes(app);
 };

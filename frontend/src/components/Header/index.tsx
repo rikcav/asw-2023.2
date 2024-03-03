915c1ae1652d91338009f730f2cd2b0dc9f91cd0
elisonmartins <martinselison10@gmail.com>
2024-03-03 14:37:39 -0300

    feat: adicionando header

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo-container">
+        <img src="logo-recanto-nordestino.png" alt="Recanto Nordestino" className="logo" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;

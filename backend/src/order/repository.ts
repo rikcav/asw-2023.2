13dfcc6886811ab269211ee860bc383ea3340257
Ronald Macario <ronaldmacario13@gmail.com>
2024-03-04 00:58:27 -0300

    feat: criando "getOrderById" no repository

--- backend/src/order/repository.ts
+++ backend/src/order/repository.ts
@@ -33,3 +33,37 @@ export const createOrder = async (data: object) => {
   return order;
 };
 
+export const getOrderById = async (id: number) => {
+  const order = await prisma.order.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+        },
+      },
+      item: {
+        select: {
+          id: true,
+          name: true,
+          category: true,
+          price: true,
+          image_url: true,
+          description: true,
+        },
+      },
+      quantity: true,
+      total: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return order;
+};

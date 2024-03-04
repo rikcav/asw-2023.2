6bb1febda628a966c450f6e344ab4e6d35a94b41
Ronald Macario <ronaldmacario13@gmail.com>
2024-03-04 00:57:48 -0300

    feat: criando "createOrder" no repository

---
+++ backend/src/order/repository.ts
@@ -0,0 +1,35 @@
+const { prisma } = require("../prisma/service.ts");
+
+export const createOrder = async (data: object) => {
+  const order = await prisma.order.create({
+    data,
+    select: {
+      id: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+        },
+      },
+      item: {
+        select: {
+          id: true,
+          name: true,
+          category: true,
+          price: true,
+          image_url: true,
+          description: true,
+        },
+      },
+      quantity: true,
+      total: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return order;
+};
+

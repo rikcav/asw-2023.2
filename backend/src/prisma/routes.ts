0f1ca502969bf676f887f12689cb8d95ca9e0360
Ronald Macario <ronaldmacario13@gmail.com>
2024-03-04 22:52:15 -0300

    feat: adicionando rotas do "Order" às rotas do prisma

--- backend/src/prisma/routes.ts
+++ backend/src/prisma/routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { userRoutes } from "../user/routes";
 import { itemRoutes } from "../item/routes";
+import { orderRoutes } from "../order/routes";
 
 module.exports = (app: express.Application) => {
   userRoutes(app);
   itemRoutes(app);
+  orderRoutes(app);
 };
